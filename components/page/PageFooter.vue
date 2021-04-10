027f657dbf2faef62a7babf6ca8de3504689d456
jchegenye <jackson.chegenye@oxygene.co.ke>
2021-04-10 12:09:56 +0300

    files & folder structure setup

---
+++ components/page/PageFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Copyright ©{{ new Date().getFullYear() }} Wine Shop
+  </a-layout-footer>
+</template>
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="container-wrapper" class="error">
+    <div>
+      <Logo />
+
+      <p
+        v-if="error.statusCode === 404"
+        class="mb-0 font-weight-300 font-size-22"
+      >
+        Page not found
+      </p>
+      <p v-else class="mb-0 font-weight-900">An error occurred</p>
+      <NuxtLink to="/">Go back home</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'base', // you can set a custom layout for the error page
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
