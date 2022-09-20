1a1030c87057121d17f8cb0747dbfbbdd46c5efc
marssamito <mwangisam671@gmail.com>
2022-09-20 19:58:56 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 export default {
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    target: 'static',
     title: 'Wine Shop',
     htmlAttrs: {
       lang: 'en',
